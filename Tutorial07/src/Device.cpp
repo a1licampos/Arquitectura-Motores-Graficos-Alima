783b867795c7965a8ffd3e8ced584763edc8a064
a1licampos <87063069+a1licampos@users.noreply.github.com>
2023-03-16 11:55:22 -0600

    Update Device.cpp
    Acomodo correcto

--- Tutorial07/src/Device.cpp
+++ Tutorial07/src/Device.cpp
@@ -18,8 +18,8 @@ Device::destroy(){
 
 HRESULT 
 Device::CreateDepthStencilView(ID3D11Resource* pResource, 
-															 const D3D11_DEPTH_STENCIL_VIEW_DESC* pDesc, 
-															 ID3D11DepthStencilView** ppDepthStencilView){
+			       const D3D11_DEPTH_STENCIL_VIEW_DESC* pDesc, 
+			       ID3D11DepthStencilView** ppDepthStencilView){
 	HRESULT hr = S_OK;
 
 	if (pResource == nullptr){
@@ -40,8 +40,8 @@ Device::CreateDepthStencilView(ID3D11Resource* pResource,
 
 HRESULT 
 Device::CreateRenderTargetView(ID3D11Resource* pResource, 
-															 const D3D11_RENDER_TARGET_VIEW_DESC* pDesc, 
-															 ID3D11RenderTargetView** ppRTView){
+			       const D3D11_RENDER_TARGET_VIEW_DESC* pDesc, 
+			       ID3D11RenderTargetView** ppRTView){
 	HRESULT hr = S_OK;
 
 	if (pResource == nullptr) {
@@ -62,8 +62,8 @@ Device::CreateRenderTargetView(ID3D11Resource* pResource,
 
 HRESULT 
 Device::CreateTexture2D(const D3D11_TEXTURE2D_DESC* pDesc, 
-												const D3D11_SUBRESOURCE_DATA* pInitialData, 
-												ID3D11Texture2D** ppTexture2D){
+			const D3D11_SUBRESOURCE_DATA* pInitialData, 
+			ID3D11Texture2D** ppTexture2D){
 	HRESULT hr = S_OK;
 
 	if (pDesc == nullptr) {
@@ -84,9 +84,9 @@ Device::CreateTexture2D(const D3D11_TEXTURE2D_DESC* pDesc,
 
 HRESULT 
 Device::CreateVertexShader(const void* pShaderBytecode, 
-													 unsigned int BytecodeLength, 
-													 ID3D11ClassLinkage* pClassLinkage, 
-													 ID3D11VertexShader** ppVertexShader){
+			   unsigned int BytecodeLength, 
+			   ID3D11ClassLinkage* pClassLinkage, 
+			   ID3D11VertexShader** ppVertexShader){
 	HRESULT hr = S_OK;
 
 	if (pShaderBytecode == nullptr) {
@@ -107,10 +107,10 @@ Device::CreateVertexShader(const void* pShaderBytecode,
 
 HRESULT 
 Device::CreateInputLayout(const D3D11_INPUT_ELEMENT_DESC* pInputElementDescs, 
-													unsigned int NumElements, 
-													const void* pShaderBytecodeWithInputSignature, 
-													unsigned int BytecodeLength, 
-													ID3D11InputLayout** ppInputLayout){
+			  unsigned int NumElements, 
+			  const void* pShaderBytecodeWithInputSignature, 
+			  unsigned int BytecodeLength, 
+			  ID3D11InputLayout** ppInputLayout){
 	HRESULT hr = S_OK;
 
 	if (pInputElementDescs == nullptr) {
@@ -127,8 +127,8 @@ Device::CreateInputLayout(const D3D11_INPUT_ELEMENT_DESC* pInputElementDescs,
 	}
 	else {
 		hr = m_device->CreateInputLayout(pInputElementDescs, NumElements, 
-																		 pShaderBytecodeWithInputSignature, 
-																		 BytecodeLength, ppInputLayout);
+						 pShaderBytecodeWithInputSignature, 
+						 BytecodeLength, ppInputLayout);
 		WARNING("Device::CreateInputLayout : [CREATION OF RESOURCE : OK] \n");
 	}
 
@@ -137,9 +137,9 @@ Device::CreateInputLayout(const D3D11_INPUT_ELEMENT_DESC* pInputElementDescs,
 
 HRESULT 
 Device::CreatePixelShader(const void* pShaderBytecode, 
-													unsigned int BytecodeLength, 
-													ID3D11ClassLinkage* pClassLinkage, 
-													ID3D11PixelShader** ppPixelShader){
+			  unsigned int BytecodeLength, 
+			  ID3D11ClassLinkage* pClassLinkage, 
+			  ID3D11PixelShader** ppPixelShader){
 	HRESULT hr;
 
 	if (pShaderBytecode == nullptr) {
@@ -160,8 +160,8 @@ Device::CreatePixelShader(const void* pShaderBytecode,
 
 HRESULT 
 Device::CreateBuffer(const D3D11_BUFFER_DESC* pDesc, 
-										 const D3D11_SUBRESOURCE_DATA* pInitialData, 
-										 ID3D11Buffer** ppBuffer){
+		     const D3D11_SUBRESOURCE_DATA* pInitialData, 
+		     ID3D11Buffer** ppBuffer){
 	HRESULT hr;
 
 	if (pDesc == nullptr) {
@@ -182,7 +182,7 @@ Device::CreateBuffer(const D3D11_BUFFER_DESC* pDesc,
 
 HRESULT 
 Device::CreateSamplerState(const D3D11_SAMPLER_DESC* pSamplerDesc, 
-													 ID3D11SamplerState** ppSamplerState){
+			   ID3D11SamplerState** ppSamplerState){
 	HRESULT hr;
 
 	if (pSamplerDesc == nullptr) {
